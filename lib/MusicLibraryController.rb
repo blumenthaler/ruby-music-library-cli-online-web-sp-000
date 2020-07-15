7a953649b2c052e918e00ab6153ae880f1e0458a
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-15 18:28:02 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -28,7 +28,8 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort{|a, b| a.name <=> b.name|.each_with_index do |
+    Song.all.sort{|a, b| a.name <=> b.name|.each_with_index do |song, index|
+      ""
   end
   
   
