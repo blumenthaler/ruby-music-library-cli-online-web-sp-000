b3a0bb2156caf983ab97b8c3aa16c4157aa5318b
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-15 17:53:18 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -52,8 +52,8 @@ class Song
   
   def self.new_by_filename(filename)
     song_info = filename.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
+    song = Song.new(song_info[1]) # still accurate?
+    song.artist_name = song_info[0] # still accurate?
     song
   end
   
