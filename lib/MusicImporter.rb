b74a92626f8bcbc9c6e90af988d60d68220f00a1
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-15 17:38:08 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -4,4 +4,9 @@ class MusicImporter
   def initialize(path)
     @path = path
   end
+  
+  def files
+    # loads all files in path directory
+    # normalizes the filename to just MP3 filename (no path)
+  end
 end
