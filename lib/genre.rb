08892ff0e7228c00607d4004b6944b659a636367
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-15 17:19:23 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -33,7 +33,7 @@ class Genre
   def artists
     @new_array = []
     @songs.each do |song|
-      if @new_array.include?(song.genre)
+      if @new_array.include?(song.artist)
         nil
       else
         @new_array << song.genre
