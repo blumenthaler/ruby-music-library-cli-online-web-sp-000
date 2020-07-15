2425bda90a84180385920c1a5fe571e44c43a9f8
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-15 18:40:11 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -60,6 +60,9 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
+    Artist.all.select do |artist|
+      if artist.name == input
+        puts artist.
   end
   
 end
