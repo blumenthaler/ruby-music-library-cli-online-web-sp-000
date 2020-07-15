8fe697b0516e23442bc2d4410b1e53094f817689
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-15 18:36:16 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -51,6 +51,11 @@ class MusicLibraryController
     end
   end
   
+  def list_genres
+    Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
+      puts "#{i+1}. #{s.name}"
+    end
+  end
   
   
 end
